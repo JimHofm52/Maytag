b75a181c1c19a00851b28300f597f07f08463566
Shreya Ravi <Shreya Ravi>
2017-02-15 18:34:11 -0500

    Added commands for gear grabber

---
+++ src/main/java/org/teamresistance/frc/command/grabber/GearRetract.java
@@ -0,0 +1,26 @@
+package org.teamresistance.frc.command.grabber;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.strongback.command.Command;
+import org.teamresistance.frc.InvertibleSolenoidWithPosition;
+
+/**
+ * Created by shrey on 2/6/2017.
+ */
+public class GearRetract extends Command {
+
+  private final InvertibleSolenoidWithPosition extendSolenoid;
+
+  public GearRetract(InvertibleSolenoidWithPosition extendSolenoid) {
+    super(extendSolenoid);
+    this.extendSolenoid = extendSolenoid;
+  }
+
+  @Override
+  public boolean execute() {
+    extendSolenoid.retract();
+    SmartDashboard.putBoolean("Gear Retracted", extendSolenoid.isRetracted());
+    return extendSolenoid.isRetracted();
+  }
+
+}
